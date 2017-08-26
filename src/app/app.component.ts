1363f85fcdf3a8e3360cb31ebe1bedd4b1d64f4f
Prateek Gogia <gogia.prateek@hotmail.com>
2017-08-26 15:21:14 +0530

    Uploaded src folder for front-end.

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+
+  constructor(private http: Http) {
+    
+  }
+
+  makecall() {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
+      console.log(res);
+    })
+  }
+}
