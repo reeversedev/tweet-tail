100805c61240d3a7bfb17a9a6f04debfe5f192de
Prateek Gogia <gogia.prateek@hotmail.com>
2017-08-26 15:19:58 +0530

    Uploaded backend stuff.

---
+++ config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: 'osquxjeWgLWhDTlxlGWI4Hx7t',
+    consumersecret: 'HJbJVxvBXl9c3QLHed6kK8buCRHujmFrdaGq2aHm1XDQ1KiAhd',
+    bearertoken: ''
+};
+
+module.exports = appsettings;
